39268a9819881b37b01618fb5123830414d76a92
rainsound-k <rainsound128@gmail.com>
2018-09-29 23:50:31 +0900

    movies 앱 admin에 등록

--- app/movies/admin.py
+++ app/movies/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Genre, Torrent, Movie
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['name',]
+
+
+@admin.register(Torrent)
+class TorrentAdmin(admin.ModelAdmin):
+    list_display = ['url', 'quality', 'size']
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'year', 'rating', 'summary']
